5f160d748684d774d9410408b7ff646858828039
clementtrecourt <clementt.pro@protonmail.com>
2026-09-24 10:00:02 +0200

    nrs: 2026-09-24 10:00:02

--- home/mango.nix
+++ home/mango.nix
@@ -48,7 +48,7 @@
       blur = 1;
       blur_layer = 1;
       blur_optimized = 0;
-      blur_params_num_passes = 3;
+      blur_params_num_passes = 2;
       blur_params_radius = 1;
       blur_params_noise = 0;
       blur_params_brightness = 1;
